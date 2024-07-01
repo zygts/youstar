311feb159e591f345024204e62bcec23dfc3fea2
Rodrigo Núñez <hi@iamrodrek.com>
2024-07-01 12:05:08 +0200

    stars grid

--- main.js
+++ main.js
@@ -6,3 +6,21 @@ import barba from "@barba/core";
 
 // Register ScrollTrigger plugin
 gsap.registerPlugin(ScrollTrigger);
+
+// Select all the image containers
+const imgContainers = document.querySelectorAll(".stars-img");
+
+// Create the animation for each image container
+imgContainers.forEach((container, index) => {
+  gsap.to(container, {
+    yPercent: 20,
+    ease: "none",
+    scrollTrigger: {
+      trigger: container,
+      start: "top bottom", // Animation starts when the top of the container reaches the bottom of the viewport
+      end: "bottom top", // Animation ends when the bottom of the container reaches the top of the viewport
+      scrub: true, // Enables smooth scrubbing
+      toggleActions: "play none none reverse", // Play animation on scroll down, reverse on scroll up
+    },
+  });
+});
